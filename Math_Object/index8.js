305c8485db02cd69e70ff9224fe5c7423463e8d2
YuvaVarshith4 <yuvavarshith4926@gmail.com>
2025-12-13 22:44:39 +0530

    Day-02 JS learning

---
+++ Math_Object/index8.js
@@ -0,0 +1,25 @@
+let x = 3.21;
+let y = 2;
+let z;
+let k = 4;
+let p = 10;
+let f = -18;
+
+// z = Math.round(x);
+// z = Math.floor(x);  //round down
+// z = Math.ceil(x);   //round up
+// z = Math.trunc(x);  //remove decimal points
+// z = Math.pow(y,k);
+// z = Math.sqrt(k);
+// z = Math.log(p);
+// z = Math.sin(45);
+// z = Math.abs(f);
+// z = Math.sign(f);   //-1 for negative, 1 for positive
+
+// console.log(z);
+
+let max = Math.max(y,k,p,f);
+let min = Math.min(y,k,p,f);
+
+console.log(max);
+console.log(min);
